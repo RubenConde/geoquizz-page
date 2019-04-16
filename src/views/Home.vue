5f786edb430fada2398ec7675d6dd90daea200b8
Rubén Conde <rubencondemag96@gmail.com>
2019-04-16 17:54:35 +0200

    About page view ready

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,42 @@
 <template>
-    <section class="hero is-fullheight-with-navbar" id="home">
-        <div class="hero-body">
-            <div class="container has-text-centered">
-                <div class="columns is-multiline is-vcentered">
-                    <div class="column is-4 is-offset-1 is-8-mobile is-offset-2-mobile animated slideInLeft">
-                        <figure class="image">
-                            <img src="../assets/img/world.png">
-                        </figure>
-                    </div>
-                    <div class="column is-offset-1 animated slideInRight">
-                        <h1 class="title" style="color: #ffffff;"> DO YOU KNOW WHERE YOU ARE?!</h1>
-                        <h2 class="subtitle" style="color: #ffffff;"> Write your name, choose the place and difficulty
-                            and PLAY!</h2>
-                        <div class="columns">
-                            <div class="column is-8 is-offset-2">
-                                <start-game-form></start-game-form>
+    <div id="home">
+        <nav-bar></nav-bar>
+        <section class="hero is-fullheight-with-navbar">
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <div class="columns is-multiline is-vcentered">
+                        <div class="column is-4 is-offset-1 is-8-mobile is-offset-2-mobile animated slideInLeft">
+                            <figure class="image">
+                                <img src="../assets/img/world.png">
+                            </figure>
+                        </div>
+                        <div class="column is-offset-1 animated slideInRight">
+                            <h1 class="title" style="color: #ffffff;"> DO YOU KNOW WHERE YOU ARE?!</h1>
+                            <h2 class="subtitle" style="color: #ffffff;"> Write your name, choose the place and
+                                difficulty
+                                and PLAY!</h2>
+                            <div class="columns">
+                                <div class="column is-8 is-offset-2">
+                                    <start-game-form></start-game-form>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+    </div>
 </template>
 
 <script>
-
+    import navBar from '../components/navBar'
     import startGameForm from "../components/startGameForm";
 
     export default {
         name: 'home',
         components: {
-            startGameForm
+            startGameForm,
+            navBar
         }
     }
 </script>
