74224e3e035594655e226a61b42f45142e874378
Rubén Conde <rubencondemag96@gmail.com>
2019-04-16 14:46:24 +0200

    Home page view ready

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,121 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <nav-bar></nav-bar>
+        <router-view/>
     </div>
-    <router-view/>
-  </div>
 </template>
+<script>
+    // @ is an alias to /src
+    import NavBar from '@/components/navBar.vue'
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+    export default {
+        name: 'app',
+        components: {
+            NavBar
+        },
+        mounted() {
+            this.getSeries();
+            this.getDifficulties();
+        }
+    }
+</script>
+<style lang="scss">
+    // Import Bulma's core
+    @import "~bulma/sass/utilities/_all";
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    // Set your colors
+    $primary: #8C0E03;
+    $primary-invert: findColorInvert($primary);
+    $info: #86A6A6;
+    $info-invert: findColorInvert($info);
+    $success: #618C03;
+    $success-invert: findColorInvert($success);
+    $warning: #F2B705;
+    $warning-invert: findColorInvert($warning);
+    $danger: #D92B04;
+    $danger-invert: findColorInvert($danger);
+    $myOrange: #D97904;
+    $myOrange-invert: findColorInvert($myOrange);
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    // Setup $colors to use as bulma classes (e.g. 'is-twitter')
+    $colors: (
+            "primary": ($primary, $primary-invert),
+            "info": ($info, $info-invert),
+            "success": ($success, $success-invert),
+            "warning": ($warning, $warning-invert),
+            "danger": ($danger, $danger-invert),
+            "myOrange": ($myOrange, $myOrange-invert)
+    );
+
+    // Links
+    $link: $primary;
+    $link-invert: $primary-invert;
+    $link-focus-border: $primary;
+
+    // Import Bulma and Buefy styles
+    @import "~bulma";
+    @import "~buefy/src/scss/buefy";
+
+    .logoText {
+        font-family: 'Pacifico', cursive;
+        font-size: 1.5rem;
+        font-weight: lighter;
+    }
+
+    #home, #about {
+        margin: 0;
+        padding: 0;
+        background: url('./assets/img/homewallpaper.jpg') repeat 0 0;
+        -webkit-animation: 10s linear 0s normal none infinite animate;
+        -moz-animation: 10s linear 0s normal none infinite animate;
+        -ms-animation: 10s linear 0s normal none infinite animate;
+        -o-animation: 10s linear 0s normal none infinite animate;
+        animation: 10s linear 0s normal none infinite animate;
+
+    }
+
+    @-webkit-keyframes animate {
+        from {
+            background-position: 0 0;
+        }
+        to {
+            background-position: 500px 0;
+        }
+    }
+
+    @-moz-keyframes animate {
+        from {
+            background-position: 0 0;
+        }
+        to {
+            background-position: 500px 0;
+        }
+    }
+
+    @-ms-keyframes animate {
+        from {
+            background-position: 0 0;
+        }
+        to {
+            background-position: 500px 0;
+        }
+    }
+
+    @-o-keyframes animate {
+        from {
+            background-position: 0 0;
+        }
+        to {
+            background-position: 500px 0;
+        }
+    }
+
+    @keyframes animate {
+        from {
+            background-position: 0 0;
+        }
+        to {
+            background-position: 500px 0;
+        }
+    }
 </style>
