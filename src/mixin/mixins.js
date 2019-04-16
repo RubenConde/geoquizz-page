ae9134fc82e467f664cd4e2412cb19a5d4df505b
Rubén Conde <rubencondemag96@gmail.com>
2019-04-16 17:53:22 +0200

    AnimateCSS method
    Method for animate elements added in mixins

--- src/mixin/mixins.js
+++ src/mixin/mixins.js
@@ -5,5 +5,20 @@ import users from './mixins/users'
 import games from './mixins/games'
 
 export default {
-    mixins: [series, photos, difficulties, users, games]
+    mixins: [series, photos, difficulties, users, games],
+    methods: {
+        animateCSS(element, animationName, callback) {
+            const node = document.querySelector(element);
+            node.classList.add('animated', animationName);
+
+            function handleAnimationEnd() {
+                node.classList.remove('animated', animationName);
+                node.removeEventListener('animationend', handleAnimationEnd);
+
+                if (typeof callback === 'function') callback()
+            }
+
+            node.addEventListener('animationend', handleAnimationEnd)
+        }
+    }
 }
