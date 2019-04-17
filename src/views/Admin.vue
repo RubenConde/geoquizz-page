11c6b7b3636047f42e0aedba22982f5c47480bf0
Rubén Conde <rubencondemag96@gmail.com>
2019-04-17 13:04:56 +0200

    Admin and login view added

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,10 +1,23 @@
 <template>
-    
+    <div id="admin">
+        <b-button @click="getUserLogged"></b-button>
+    </div>
 </template>
 
 <script>
+    import BButton from "buefy/src/components/button/Button";
+
     export default {
-        name: "Admin"
+        name: "Admin",
+        components: {BButton},
+        methods: {
+            getUserLogged() {
+                this.$store.dispatch('GET_USER')
+                    .then(resp => {
+                        console.log(resp)
+                    })
+            }
+        }
     }
 </script>
 
---
+++ src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="hero is-info is-fullheight">
+        <div class="hero-body">
+            <div class="container">
+                <div class="columns">
+                    <div class="column is-one-third is-offset-one-third is-full-mobile">
+                        <login-form></login-form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    import loginForm from '../components/loginForm'
+
+    export default {
+        name: "Login",
+        components: {
+            loginForm
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
