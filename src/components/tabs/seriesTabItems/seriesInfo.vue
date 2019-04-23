36a005563c7738de9234e11195ccf0ee9d56d633
Rubén Conde <rubencondemag96@gmail.com>
2019-04-23 19:11:08 +0200

    Series info modal

---
+++ src/components/tabs/seriesTabItems/seriesInfo.vue
@@ -0,0 +1,109 @@
+<template>
+
+    <div class="modal-card">
+        <header class="modal-card-head is-small">
+            <p class="modal-card-title">Series information</p>
+        </header>
+        <section class="modal-card-body">
+            <div class="columns is-multiline">
+                <div class="column">
+                    <b-field>
+                        <gmap-autocomplete :value="seriesChanged.city" @place_changed="setPlace" class="input is-small">
+                        </gmap-autocomplete>
+                    </b-field>
+                </div>
+                <div class="column is-1">
+                    <b-tooltip :label="'This place has ' + selectedSeries.photos.length + ' photo(s)'" position="is-left">
+                        <b-icon icon="image-multiple" ></b-icon>
+                    </b-tooltip>
+                </div>
+                <div class="column is-1">
+                    <b-tooltip :label="'This place has ' + selectedSeries.games.length + ' game(s)'" position="is-left">
+                        <b-icon icon="gamepad-variant" ></b-icon>
+                    </b-tooltip>
+                </div>
+            </div>
+            <gmap-map :center="{lat : seriesChanged.latitude, lng : seriesChanged.longitude}" :options="mapOptions"
+                      :zoom="seriesChanged.zoom" @zoom_changed="changeZoomLevel" class="map">
+                <GmapMarker :draggable="true" :position="{lat : seriesChanged.latitude, lng : seriesChanged.longitude}"
+                            @dragend="changeMarkerPosition"/>
+            </gmap-map>
+        </section>
+        <footer class="modal-card-foot">
+            <b-button @click="$parent.close()" size="is-small" type="is-danger">Cancel</b-button>
+            <b-button @click="modifSeries" size="is-small" type="is-success">Save changes</b-button>
+        </footer>
+    </div>
+
+</template>
+
+<script>
+    import BButton from "buefy/src/components/button/Button";
+    import BField from "buefy/src/components/field/Field";
+    import BIcon from "buefy/src/components/icon/Icon";
+    import BTooltip from "buefy/src/components/tooltip/Tooltip";
+
+    export default {
+        name: "seriesInfo",
+        components: {BTooltip, BIcon, BField, BButton},
+        props: ['selectedSeries'],
+        data() {
+            return {
+                mapOptions: {
+                    controlSize: 20,
+                    draggable: true,
+                    zoomControl: true,
+                    mapTypeControl: true,
+                    scaleControl: false,
+                    streetViewControl: false,
+                    rotateControl: false,
+                    fullscreenControl: false,
+                },
+                seriesChanged: {}
+            }
+        },
+        mounted() {
+            this.seriesChanged = this.selectedSeries;
+        },
+        methods: {
+            setPlace(place) {
+                let placePos = place.geometry.location.toJSON();
+                this.seriesChanged.city = place.formatted_address;
+                this.seriesChanged.latitude = placePos.lat;
+                this.seriesChanged.longitude = placePos.lng
+            },
+            changeMarkerPosition(e) {
+                let newPos = e.latLng.toJSON();
+                this.seriesChanged.latitude = newPos.lat;
+                this.seriesChanged.longitude = newPos.lng
+            },
+            changeZoomLevel(e) {
+                this.seriesChanged.zoom = e
+            },
+            async modifSeries() {
+                let seriesUpdated = {
+                    id: this.seriesChanged.id,
+                    city: this.seriesChanged.city,
+                    latitude: this.seriesChanged.latitude,
+                    longitude: this.seriesChanged.longitude,
+                    zoom: this.seriesChanged.zoom
+                };
+                await this.$store.dispatch('UPDATE_SERIES', seriesUpdated)
+                    .then(async response => {
+                        if (response.success) {
+                            await this.getInfo();
+                            this.showSuccess(response.message);
+                        } else
+                            this.showError(response.message)
+                    })
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .map {
+        width: 100%;
+        height: 300px;
+    }
+</style>
